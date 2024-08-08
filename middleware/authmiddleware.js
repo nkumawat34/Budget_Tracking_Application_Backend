31a898b7bacc2ab1ec3068055c8bff94e836d22f
Neeraj Kumawat <nkumawat34@gmail.com>
2024-08-08 09:42:31 -0700

    Added Auhorization

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -8,10 +8,12 @@ const authenticateJWT = (req, res, next) => {
 
   // Extract the token
   const token = authHeader.split(' ')[1];
+
   if (!token) return res.sendStatus(401);
 
   // Verify the token
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+    console.log(err)
     if (err) return res.sendStatus(403); // Forbidden
     req.user = user; // Attach the user info to the request object
     next(); // Proceed to the next middleware or route handler
